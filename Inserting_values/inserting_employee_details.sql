cb80e2c34e7d1a20304aa3f7ca948ad441a6596f
Nithin K P <nithinkp0212@gmail.com>
2025-06-01 17:42:07 +0530

    Inserting Values to employee_details table

---
+++ Inserting_values/inserting_employee_details.sql
@@ -0,0 +1,6 @@
+INSERT INTO employee_details(Name, Address, Age) VALUES ('Nithin K P','Kodagu,Karnataka','21');
+INSERT INTO employee_details(Name, Address, Age) VALUES ('Madan Gowda G R','Mandya,Karnataka','20');
+INSERT INTO employee_details(Name, Address, Age) VALUES ('Gagan','Kodagu,Karnataka','22');
+INSERT INTO employee_details(Name, Address, Age) VALUES ('Rahul Gowda','Mysore,Karnataka','19');
+
+SELECT * FROM employee_details;
