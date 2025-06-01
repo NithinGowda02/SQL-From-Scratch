aada82f26087adc5912444285501e2bc559cd42f
Nithin K P <nithinkp0212@gmail.com>
2025-06-01 18:17:59 +0530

    Inserting values to department_table

---
+++ Inserting_values/inserting_department.sql
@@ -0,0 +1,5 @@
+INSERT INTO department(EMP_ID, EMP_DEPARTMENT, EMAIL) VALUES ('Developer','nithinkp368@gmail.com');
+INSERT INTO department(EMP_ID, EMP_DEPARTMENT) VALUES ('Tester');
+INSERT INTO department(EMP_ID, EMP_DEPARTMENT, EMAIL) VALUES ('Data Analyst','nithinkp368@gmail.com');
+
+SELECT * FROM department;
