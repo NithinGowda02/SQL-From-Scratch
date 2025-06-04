91c1a4222c320ff342a08eedc3e8396c9935edf2
Nithin K P <nithinkp0212@gmail.com>
2025-06-04 20:46:58 +0530

    Solving simple problem with concat method

---
+++ Concat_Method/example.sql
@@ -0,0 +1,5 @@
+/*Task
+	1.Give me full name of all students
+  2.Give me full name and login count of all users
+    */
+SELECT concat(std_FirstName, std_LastName) as Full_Name, login_count FROM student;   
