044cb9e5eba27a86361db2baa17e3d79dd4209f4
Nithin K P <nithinkp0212@gmail.com>
2025-06-06 21:14:05 +0530

    Inserting Values to student table of collage database

---
+++ inserting_values/example.sql
@@ -0,0 +1,8 @@
+INSERT INTO student(USN,name,section,marks,city) values ("4MH22CS082","Madhan Gowda G R","B",85,"Mandya"),
+														                            ("4MH22CS083","Mahesha K","B",93,"Piriyapatns"),
+                                                        ("4MH22CS086","Manvith M N","B",74,"Mandya"),
+                                                        ("4MH22CS111","Prajwal H N","B",81,"Mandya"),
+                                                        ("4MH23CS402","Bharath Kumar Naik","B",76,"Davanagere"),
+                                                        ("4MH22CS409","Nithin K P","B",92,"Madikeri"),
+                                                        ("4MH22CS028","Deeekshith Naik S","B",82,"HD kote"),
+                                                        ("4MH22CS027","Deekshith I K","B",87,"Narsipura");
