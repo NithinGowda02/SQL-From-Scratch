65bae91049dcc816a7803491b1049340f642c17b
Nithin K P <nithinkp0212@gmail.com>
2025-06-09 22:40:22 +0530

    creating a table teacher table using foreign key and cascading method

--- collage/teacher.sql
+++ collage/teacher.sql
@@ -1,4 +1,6 @@
 CREATE TABLE teacher(id INT PRIMARY KEY,
-                    name VARCHAR(50) NOT NULL,
-                    department_id int NOT NULL,
-                    FOREIGN KEY(department_id) REFERENCES department(dept_id));
+                     name VARCHAR(50) NOT NULL,
+                     department_id INT NOT NULL,
+                     FOREIGN KEY(department_id) REFERENCES department(dept_id)
+                   	 ON UPDATE CASCADE
+                     ON DELETE CASCADE);
