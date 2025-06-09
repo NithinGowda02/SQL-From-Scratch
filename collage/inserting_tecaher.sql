4bc71347a9302a20caeb0654f7bdc52c8037d00f
Nithin K P <nithinkp0212@gmail.com>
2025-06-09 22:16:22 +0530

    Inserting values to teacher table

--- collage/inserting_tecaher.sql
+++ collage/inserting_tecaher.sql
@@ -1,5 +1,5 @@
 INSERT INTO teacher(id,name,department_id) VALUES (1,"Pallavi",101),
-											                        	(2,"Akshatha",102),
+						(2,"Akshatha",102),
                                                 (3,"Sathish",105),
                                                 (4,"Santhosh",101),
                                                 (5,"Akshath",103),
