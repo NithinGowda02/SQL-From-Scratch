fe159ee2dc2f8407815fe3fc738d99900dff3f6e
Nithin K P <nithinkp0212@gmail.com>
2025-06-09 21:55:56 +0530

    Inserting values to department table

---
+++ collage/inserting_Values_department.sql
@@ -0,0 +1,6 @@
+INSERT INTO department(dept_id, name) VALUES (101,"CSE"),
+											                      (102,"ECE"),
+                                            (103,"IS"),
+                                            (104,"AI"),
+                                            (105,"ME"); 
+SELECT * FROM department;      
