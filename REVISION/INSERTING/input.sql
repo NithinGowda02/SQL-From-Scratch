cbe6fc55193791edec47c53988281d7fa9dcc29d
Nithin K P <nithinkp0212@gmail.com>
2025-06-17 21:19:58 +0530

    Inserting Values to student table

---
+++ REVISION/INSERTING/input.sql
@@ -0,0 +1,8 @@
+INSERT INTO student(ROLL_NO,NAME,CLASS,DOB,GENDER,CITY,MARKS) VALUES(1,"AMITH P M","VII",13-06-2006,"MALE","SULYA",76.05),
+																	                                  (2,"AMRUTHESH","VI",26-12-2007,"MALE","MYSORE",84.75),
+                                                                    (3,"BINDU U S","V",23-08-2008,"FEMALE","MYSORE",56.45),
+                                                                    (4,"CHANDAN K","VI",08-03-2006,"MALE","SOMWARPET",46.05),
+                                                                    (5,"DHANUSH K ","VII",17-06-2007,"MALE","PUTTUR",73.97),
+                                                                    (6,"MEGHAN N G","IX",21-07-2005,"MALE","SAKALESHPUR",65.78),
+                                                                    (7,"NITHIN K P","X",02-12-2002,"MALE","SOMWARPET",92.76),
+                                                                    (8,"SWAGATH K U","X",01-07-2002,"MALE","SOMWARPET",86.89);
